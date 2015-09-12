af4da16d93fc344256a025176d5bce5e5f95126e
Bo Jeanes <me@bjeanes.com>
2015-09-12 14:23:21 +1000

    Convert to literate org

--- project.clj
+++ project.clj
@@ -3,4 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :plugins [[lein-cljsbuild "1.0.6"]
+            [lein-figwheel "0.3.8-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {:builds {:dev {:source-paths ["src"]
+                             :figwheel true
+                             :compiler {:output-to "mujic.js"
+                                        :output-dir "out"
+                                        :main "mujic"
+                                        :asset-path "out"
+                                        :optimizations :none
+                                        :recompile-dependents true
+                                        :source-map-timestamp true
+                                        :source-map true}} }})
