ec1d11d68a8a31dfafa79d13d09d5c15e9cdc22e
Bo Jeanes <me@bjeanes.com>
2015-09-23 18:54:53 +1000

    Use emacs dir locals for some of the project config

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-export-html-style-include-scripts)
+  (org-export-html-style-include-default)
+  (org-export-htmlize-output-type 'css)
+  (org-babel-clojure-backend 'cider)))
